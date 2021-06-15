593946440864a5efd6788fbc5a6a1fc22c6498e8
Paulina Binek <binek.paulina93@gmail.com>
2021-06-15 22:03:04 +0200

    Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from FirstClass");
+        System.out.println("Paulina");
+        System.out.println(28);
+        System.out.println('B');
+    }
+}
