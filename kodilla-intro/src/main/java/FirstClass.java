7ccfa84248ee7a89b72e78a6af204a803ae49913
Paulina Binek <binek.paulina93@gmail.com>
2021-07-18 21:35:04 +0200

    FirstClassNotebook

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,11 +1,25 @@
 public class FirstClass {
-public static void main(String[] args) {
-    String helloFrom = "Hello from FirstClass!";
-    System.out.println("Hello from FirstClass!");
-    System.out.println(helloFrom);
+        public static void main(String[] args) {
 
-    if (2 > 1){
-        System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
-}
+            Notebook notebook = new Notebook(200, 1500, 2018);
+            System.out.println(notebook.weight + " " + notebook.price);
+            notebook.checkPrice();
+            notebook.checkWeight();
+            notebook.checkPurchase();
 
-}}
+            Notebook heavyNotebook = new Notebook(700, 1300, 2000);
+            System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+            heavyNotebook.checkPrice();
+            heavyNotebook.checkWeight();
+            heavyNotebook.checkPurchase();
+
+            Notebook oldNotebook = new Notebook(1600,650, 2019);
+            System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+            oldNotebook.checkPrice();
+            oldNotebook.checkWeight();
+            oldNotebook.checkPurchase();
+
+        }
+    }
+
+
