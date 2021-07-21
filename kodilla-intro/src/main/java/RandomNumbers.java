5c515a830150b5ac5b8991561bc4583360784366
Paulina Binek <binek.paulina93@gmail.com>
2021-07-21 19:20:08 +0200

    RandomNumbers

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class RandomNumbers {
+import java.util.Random;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] randomElements = new int[10];
+
+        int sum=0;
+        for (int i=0; i < randomElements.length; i++)
+        {
+            randomElements[i] = rand.nextInt(31);
+            sum += randomElements[i];
+
+            if (sum >= 5000) {
+                return;
+            }
+
+        }
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Minimum Value = " + getMinValue(randomElements));
+        System.out.println("Maximum Value = " + getMaxValue(randomElements));
+
+    }
+    public static int getMaxValue(int[] randomElements){
+        int maxValue = randomElements[0];
+        for(int i=1;i<randomElements.length;i++){
+            if(randomElements[i] > maxValue){
+                maxValue = randomElements[i];
+            }
+        }
+        return maxValue;
+    }
+
+    public static int getMinValue(int[] randomElements){
+        int minValue = randomElements[0];
+        for(int i=1;i<randomElements.length;i++){
+            if(randomElements[i] < minValue){
+                minValue = randomElements[i];
+            }
+        }
+        return minValue;
+    }
+
 }
