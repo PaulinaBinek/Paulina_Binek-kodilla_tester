47836c47e2c863604c552b48889ec9c381e162ee
Paulina Binek <binek.paulina93@gmail.com>
2021-07-25 15:57:47 +0200

    .

---
+++ kodilla-intro/src/main/java/Loops.java
@@ -0,0 +1,15 @@
+public class Loops {
+    public static void main(String[] args) {
+    int [] numbers = new int[] {1, 2, 3, 4};
+    int result = sumNumbers(numbers);
+        System.out.println(result);
+    }
+
+    public static int sumNumbers(int[] numbers) {
+        int result = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            result = result + numbers[i];
+        }
+        return result;
+    }
+}
--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -4,25 +4,26 @@ public class RandomNumbers {
     public static void main(String[] args) {
         Random rand = new Random();
         int sum = 0;
-        int numbers = 300;
         int min = rand.nextInt(31);
         int max = min;
         sum = sum + max;
 
-        for (int i = 0; i < numbers; i++) {
+        while (sum <= 5000) {
             int random = rand.nextInt(31);
             sum = sum + random;
+            sum++;
+
 
             if (max < random) max = random;
-            if(random < min) min = random;
+            if (random < min) min = random;
 
-            if (sum >= 5000) {
-                return;
-            }
 
             System.out.println("Random number is: " + random);
             System.out.println("Max value is: " + max);
             System.out.println("Min value is: " + min);
             System.out.println("The Sum is: " + sum);
 
-        }}}
+        }
+
+    }
+}
