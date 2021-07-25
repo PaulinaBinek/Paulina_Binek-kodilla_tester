d5b72d9661a504448308ea78b66687a400203afa
Paulina Binek <binek.paulina93@gmail.com>
2021-07-25 15:49:04 +0200

    Application

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -66,9 +66,5 @@ public class Application {
     }
 }
 
-        public static void main(String[] args) {
-            Application adam = new Application("Adam", 40.5, 178);
-            adam.User();
-        }
-    }
+
 
